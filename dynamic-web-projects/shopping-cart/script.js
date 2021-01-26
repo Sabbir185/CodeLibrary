ab19685af88e39b9251ee2ba57a38a67d0732d2e
unknown <sabbir.cse@yahoo.com>
2021-01-27 01:10:07 +0600

    minus working

--- dynamic-web-projects/shopping-cart/script.js
+++ dynamic-web-projects/shopping-cart/script.js
@@ -10,3 +10,16 @@ plusBtn.addEventListener("click", function(){
    document.getElementById('numOfItem').value = count;
    console.log( count);
 })
+
+const minusBtn = document.getElementById('minusBtn');
+let cnt = 0;
+minusBtn.addEventListener("click", function(){
+    
+   const numOfItem = document.getElementById('numOfItem').value;
+   const convertItem = parseInt(numOfItem);
+
+   cnt = convertItem - 1;
+
+   document.getElementById('numOfItem').value = cnt;
+   console.log( cnt);
+})
