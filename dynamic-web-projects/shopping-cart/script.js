8defe50e2fe03ef564934749a8a52163636e64b9
unknown <sabbir.cse@yahoo.com>
2021-01-27 10:49:00 +0600

    plus minus function base ok

--- dynamic-web-projects/shopping-cart/script.js
+++ dynamic-web-projects/shopping-cart/script.js
@@ -1,25 +1,29 @@
+// plus button
 const plusBtn = document.getElementById('plusBtn');
-let count = 0;
 plusBtn.addEventListener("click", function(){
-    
-   const numOfItem = document.getElementById('numOfItem').value;
-   const convertItem = parseInt(numOfItem);
-
-   count = convertItem + 1;
-
-   document.getElementById('numOfItem').value = count;
-   console.log( count);
+    plusAction("numOfItem");
 })
 
+// minus button
 const minusBtn = document.getElementById('minusBtn');
-let cnt = 0;
 minusBtn.addEventListener("click", function(){
-    
-   const numOfItem = document.getElementById('numOfItem').value;
-   const convertItem = parseInt(numOfItem);
-
-   cnt = convertItem - 1;
-
-   document.getElementById('numOfItem').value = cnt;
-   console.log( cnt);
+    minusAction("numOfItem");
 })
+
+
+let cnt = 0;
+function plusAction(id)
+{   
+    const numOfItem = document.getElementById(id).value;
+    const convertItem = parseInt(numOfItem);
+    cnt = convertItem + 1;
+    document.getElementById(id).value = cnt;
+    console.log(cnt)
+}
+function minusAction(id)
+{   
+    const numOfItem = document.getElementById(id).value;
+    const convertItem = parseInt(numOfItem);
+    cnt = convertItem - 1;
+    document.getElementById(id).value = cnt;
+}
