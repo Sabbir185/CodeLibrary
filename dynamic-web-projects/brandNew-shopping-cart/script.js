26d4c736be0a0d3c7de11279a8ba3039c8a28439
unknown <sabbir.cse@yahoo.com>
2021-01-28 21:19:28 +0600

    generic function

--- dynamic-web-projects/brandNew-shopping-cart/script.js
+++ dynamic-web-projects/brandNew-shopping-cart/script.js
@@ -1,15 +1,38 @@
 
-function productChangeCount(isIncrease){
-    const caseQuantity = document.getElementById("case-quantity");
-    const caseCount = parseInt(caseQuantity.value);
-    let caseNewCount = caseCount;
+function productChangeCount(product , isIncrease){
+    const productQuantity = document.getElementById(product+"-quantity");
+    const productCount = parseInt(productQuantity.value);
+    let productNewCount = productCount;
     if(isIncrease == true ){
-        caseNewCount = caseCount + 1;
+        productNewCount = productCount + 1;
     }
-    if(isIncrease == false && caseNewCount > 0 ){
-        caseNewCount = caseCount - 1;
+    if(isIncrease == false && productNewCount > 0 ){
+        productNewCount = productCount - 1;
     }
-    caseQuantity.value = caseNewCount;
-    let caseCost = caseNewCount * 59;
-    document.getElementById("caseCost").innerText = caseCost;
+    productQuantity.value = productNewCount;
+    let productCost = 0;
+    if(product == 'phone'){
+        productCost = productNewCount * 1219;
+    }
+    if(product == 'case'){
+        productCost = productNewCount * 59;
+    }
+
+    document.getElementById(product+"Cost").innerText = productCost;
 }
+
+
+// function productChangeCount(isIncrease){
+//     const caseQuantity = document.getElementById("case-quantity");
+//     const caseCount = parseInt(caseQuantity.value);
+//     let caseNewCount = caseCount;
+//     if(isIncrease == true ){
+//         caseNewCount = caseCount + 1;
+//     }
+//     if(isIncrease == false && caseNewCount > 0 ){
+//         caseNewCount = caseCount - 1;
+//     }
+//     caseQuantity.value = caseNewCount;
+//     let caseCost = caseNewCount * 59;
+//     document.getElementById("caseCost").innerText = caseCost;
+// }
