e44373132bd8627ee28bdde094638ff90b2f5777
unknown <sabbir.cse@yahoo.com>
2021-01-28 19:58:01 +0600

    code reduce using function

--- dynamic-web-projects/brandNew-shopping-cart/script.js
+++ dynamic-web-projects/brandNew-shopping-cart/script.js
@@ -1,22 +1,25 @@
 document.getElementById("case-increase").addEventListener("click", function(){
 
-    const caseQuantity = document.getElementById("case-quantity");
-    const caseCount = parseInt( caseQuantity.value );
-    let caseIncrease = caseCount + 1 ;
-    caseQuantity.value = caseIncrease ;
-    let caseCost = caseIncrease * 59 ;
+    productChangeCount(true);
 
-    document.getElementById("caseCost").innerText = caseCost;
-
-    
-
-})
+});
 
 document.getElementById("case-decrease").addEventListener("click",function(){
+    productChangeCount(false);
+});
+
+function productChangeCount(isIncrease){
     const caseQuantity = document.getElementById("case-quantity");
     const caseCount = parseInt(caseQuantity.value);
-    let caseDecrease = caseCount - 1;
-    caseQuantity.value = caseDecrease;
-    let caseCost = caseDecrease * 59;
+    // let caseDecrease = caseCount - 1;
+    let caseNewCount = caseCount;
+    if(isIncrease == true ){
+        caseNewCount = caseCount + 1;
+    }
+    if(isIncrease == false && caseNewCount > 0 ){
+        caseNewCount = caseCount - 1;
+    }
+    caseQuantity.value = caseNewCount;
+    let caseCost = caseNewCount * 59;
     document.getElementById("caseCost").innerText = caseCost;
-})
+}
