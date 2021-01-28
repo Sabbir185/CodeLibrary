1fcade8642a82b6f19c8b67e1cba2ce22c2141ac
unknown <sabbir.cse@yahoo.com>
2021-01-28 20:01:36 +0600

    again code reduce using onclick method

--- dynamic-web-projects/brandNew-shopping-cart/script.js
+++ dynamic-web-projects/brandNew-shopping-cart/script.js
@@ -1,17 +1,7 @@
-document.getElementById("case-increase").addEventListener("click", function(){
-
-    productChangeCount(true);
-
-});
-
-document.getElementById("case-decrease").addEventListener("click",function(){
-    productChangeCount(false);
-});
 
 function productChangeCount(isIncrease){
     const caseQuantity = document.getElementById("case-quantity");
     const caseCount = parseInt(caseQuantity.value);
-    // let caseDecrease = caseCount - 1;
     let caseNewCount = caseCount;
     if(isIncrease == true ){
         caseNewCount = caseCount + 1;
