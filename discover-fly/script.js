7e40560797676b0d4e872d47afc826b5ae855f36
unknown <sabbir.cse@yahoo.com>
2021-01-30 13:37:03 +0600

    final version

--- discover-fly/script.js
+++ discover-fly/script.js
@@ -19,7 +19,7 @@ function counterHandler(className,counterButton){
     let taxCost = subTotal * 0.1      // 10% tax
     document.getElementById('taxPay').innerText = taxCost ;
 
-    let totalCost = taxCost + subTotal;
+    let totalCost = taxCost + subTotal;  // total tickets cost
     document.getElementById('totalCost').innerText = totalCost ;
 
 }
@@ -36,3 +36,51 @@ function subTotalCalculate(){
 
     return subTotal;
 }
+
+
+
+// customization book now button
+document.getElementById('bookNowBtn').addEventListener("click",function(){
+    document.querySelector(".pop-up").style.display = "flex";
+    document.querySelector(".popup-content").style.display = "block";
+
+    buttonHide();
+    displayContentShow();
+
+})
+
+
+// pick up information from above function / html elements 
+// and display some values on pop-up window
+function displayContentShow(){ 
+
+   const flyInput = document.getElementById("flying-input").value;
+   document.getElementById('fromInput').innerText = flyInput;
+
+   const destinationInput = document.getElementById("destinationInput").value;
+   document.getElementById("toInput").innerText = destinationInput;
+
+   document.getElementById("firstClassTicket").innerText = document.getElementById("firstClassInput").value;
+
+   document.getElementById("economyClassTicket").innerText = document.getElementById("economyClassInput").value;
+
+   document.getElementById("totalPopUp").innerText = document.getElementById("totalCost").innerText;
+}
+
+
+function buttonHide(){
+    document.getElementById("btnHide1").style.display = "none";
+    document.getElementById("btnHide2").style.display = "none";
+    document.getElementById("btnHide3").style.display = "none";
+    document.getElementById("btnHide4").style.display = "none";
+}
+
+
+document.getElementById("close").addEventListener("click", function(){
+    document.querySelector(".pop-up").style.display = "none";
+
+    document.getElementById("btnHide1").style.display = "inline-block";
+    document.getElementById("btnHide2").style.display = "inline-block";
+    document.getElementById("btnHide3").style.display = "inline-block";
+    document.getElementById("btnHide4").style.display = "inline-block";
+})
