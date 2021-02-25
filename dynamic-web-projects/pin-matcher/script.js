5c9852bf3ca9c72e70696a1ee0b27b1cf5d983f7
unknown <sabbir.cse@yahoo.com>
2021-02-25 17:06:33 +0600

    initial matching done

--- dynamic-web-projects/pin-matcher/script.js
+++ dynamic-web-projects/pin-matcher/script.js
@@ -1,4 +1,34 @@
 function randomNumber(){
     let randNumber = (Math.random()*10000+'').split('.')[0];
+    
+    while(randNumber.length != 4){
+        randNumber = (Math.random()*10000+'').split('.')[0];
+    }
+    
     document.getElementById('generate-display').value = randNumber;
 }
+
+
+document.getElementById('digits-container').addEventListener("click",function(event){
+    const digit = event.target.innerText;
+    if(isNaN(digit)){
+        //handle
+    }
+    else{
+        document.getElementById('user-input').value += digit;
+    }
+});
+
+
+function verifyPin(){
+    const userPin = document.getElementById('user-input').value;
+    const randomPin = document.getElementById('generate-display').value;
+
+    if( userPin===randomPin){
+        alert("successfull !");
+    }
+    else{
+        alert("Bad !");
+    }
+}
+
