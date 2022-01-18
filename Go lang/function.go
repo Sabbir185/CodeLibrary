1d65f4ecd33290981f33243bee3573d48ec0e870
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-01-18 22:39:47 +0600

    multiple return value from function in golang

--- Go lang/function.go
+++ Go lang/function.go
@@ -11,10 +11,17 @@ func addition(a, b, c int) int {
 	return a + b + c
 }
 
+func mutlipleReturnValue() (int, int) {
+	return 3, 7
+}
+
 
 func main(){
 	res := doSum(1, 2)
 	res2 := addition(1, 2, 3)
 
 	fmt.Println("doSum : ", res,"addition : ", res2)
+
+	a, b := mutlipleReturnValue()
+	fmt.Println(a, b)
 }
